f7847b8a1a64a032ba0b8bdc98a0a6157387d6a3
benhassenkarim <104921495+benhassenkarim@users.noreply.github.com>
2022-06-22 08:46:49 +0200

    Create todoManagerApplication.java

---
+++ src/main/java/com/todoManager/todoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.todoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class todoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(todoManagerApplication.class, args);
+	}
+
+}
